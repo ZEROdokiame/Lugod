c4944acb5e69cab33e92ba3e365cc21ea930079f
吴彭 <wupeng@anhhg.com>
2024-09-22 22:20:36 +0800

    添加商户、渠道、用户信息。修改部分配置文件

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/domain/http/Merchant.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/domain/http/Merchant.java
@@ -109,6 +109,10 @@ public class Merchant extends BaseEntity
     @Excel(name = "标签")
     private String label;
 
+    /** 商户是否开启二要素     0 否 1 是 */
+    @Excel(name = "商户是否开启二要素")
+    private Boolean merchantAuth;
+
     /** 无社保 */
     @Excel(name = "无社保")
     private Boolean socialSecurityNo;
